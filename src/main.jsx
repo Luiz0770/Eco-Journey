703eba6cb3284f06a8954e8e3ded130dc0dd8a55
Luiz Felipe <ramosluizfelipe104@gmail.com>
2024-11-13 20:36:05 -0300

    Criando Hero

---
+++ src/main.jsx
@@ -0,0 +1,26 @@
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import './index.css';
+
+// Imports
+import App from './App.jsx';
+import Home from './pages/Home.jsx';
+import PageNotFound from './pages/PageNotFound.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      { index: true, element: <Home /> },
+      { path: "*", element: <PageNotFound /> },
+    ],
+  },
+]);
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router} future={{ v7_startTransition: true }} />
+  </StrictMode>,
+);
---
+++ src/pages/PageNotFound.jsx
@@ -0,0 +1,7 @@
+export default function PageNotFound() {
+    return ( 
+        <>
+            <h1 className="text-center">Pagina nao encontrado!</h1>
+        </>
+     );
+}
